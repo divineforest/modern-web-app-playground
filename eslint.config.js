a8a01728397d287daeda79ec77a5eafe194805db
Alexander Balashov <divineforest@gmail.com>
2026-01-12 17:18:51 +0100

    fix: enable no-misused-promises

--- eslint.config.js
+++ eslint.config.js
@@ -37,7 +37,6 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-misused-promises': 'off',
     },
   }
 );
